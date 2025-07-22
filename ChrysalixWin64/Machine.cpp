04e353c98739d41f1ccc9a8d4a859d9266fcff60
Genny <vionole@gmail.com>
2025-07-22 20:19:26 +0300

    Разделение реализации и заголовка функций - инструкций

--- ChrysalixWin64/Machine.cpp
+++ ChrysalixWin64/Machine.cpp
@@ -10,11 +10,12 @@ Machine::Machine(std::map<std::wstring, Var> in, bool dbg) {
 }
 
 void Machine::prepare() {
+	InstructionMap inst;
 	this->instruct_count = (int)instructions.size();
 	this->instruct_number = 0;
 	for (unsigned int i = 0; i < this->instruct_count; ++i) {
 		try {
-			functions[this->instructions[i].opCode](this, &instructions[i], true, true);
+			inst.functions[this->instructions[i].opCode](this, &instructions[i], true, true);
 		}
 		catch (const std::wstring& error_message) {
 			throw std::wstring{ LangLib::getTrans(L"Ошибка выполнения инструкции ") + std::to_wstring(this->instruct_number + 1) + L" (" + this->instructions[i].as_string + L"): " + error_message };
@@ -24,13 +25,14 @@ void Machine::prepare() {
 }
 
 Var Machine::go() {
+	InstructionMap inst;
 	while (this->instruct_number != -1) {
 		if (this->instruct_number >= this->instruct_count) {
 			throw std::wstring{ LangLib::getTrans(L"Ошибка после выполнения инструкции ") + std::to_wstring(this->instruct_number) + L" (" + this->instructions[(long long int)this->instruct_number - 1].as_string + LangLib::getTrans(L"): Неожиданный конец программы. Пропущена инструкция END\n") };
 		}
 
 		try {
-			functions[this->instructions[this->instruct_number].opCode](this, &instructions[this->instruct_number], false, false);
+			inst.functions[this->instructions[this->instruct_number].opCode](this, &instructions[this->instruct_number], false, false);
 		}
 		catch (const std::wstring& error_message) {
 				throw std::wstring{ LangLib::getTrans(L"Ошибка выполнения инструкции ") + std::to_wstring(this->instruct_number + 1) + L" (" + this->instructions[this->instruct_number].as_string + L"): " + error_message };
